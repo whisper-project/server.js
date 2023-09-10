9933e79ffb591e2db0370b7710412c24b92998bc
Daniel Brotsky <dan@clickonetwo.io>
2023-09-10 08:26:27 -0400

    Clean up route handling.
    Optionally accept more data from client calls.

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -10,18 +10,19 @@ import {createAblyPublishTokenRequest, createAblySubscribeTokenRequest, validate
 import {randomUUID} from 'crypto'
 
 export async function apnsToken(req: express.Request, res: express.Response)  {
-    const body = req.body
+    const body: { [p: string]: string } = req.body
     if (!body?.token || !body?.deviceId || !body?.clientId || !body?.lastSecret) {
         console.log(`Missing key in posted apnsToken body: ${JSON.stringify(body)}`)
         res.status(400).send({ status: 'error', reason: 'Invalid post data' });
         return
     }
-    const clientKey = `cli:${body.clientId}`
-    const tokenHex = Buffer.from(body.token, 'base64').toString('hex')
-    const secretHex = Buffer.from(body.lastSecret, 'base64').toString('hex')
+    const { token, deviceId, clientId, lastSecret } = body
+    const clientKey = `cli:${clientId}`
+    const tokenHex = Buffer.from(token, 'base64').toString('hex')
+    const secretHex = Buffer.from(lastSecret, 'base64').toString('hex')
     const received: ClientData = {
-        id: body.clientId,
-        deviceId: body.deviceId,
+        id: clientId,
+        deviceId,
         token: tokenHex,
         tokenDate: Date.now(),
         lastSecret: secretHex,
@@ -31,7 +32,12 @@ export async function apnsToken(req: express.Request, res: express.Response)  {
     const existing = await getClientData(clientKey)
     // see refreshSecret for explanation of logic around lastSecret
     let clientChanged = !existing || received.lastSecret !== existing?.lastSecret
-    clientChanged = clientChanged || received.token !== existing?.token || received.deviceId !== existing?.deviceId
+    if (received.token !== existing?.token || received.deviceId !== existing?.deviceId) {
+        clientChanged = true
+    }
+    if (received.userName !== existing?.userName || received.appInfo !== existing?.appInfo) {
+        clientChanged = true
+    }
     const appInfo = body?.appInfo ? ` (${body.appInfo})` : ''
     if (clientChanged) {
         console.log(`Received APNS token from new or changed client ${clientKey}${appInfo}`)
@@ -44,34 +50,32 @@ export async function apnsToken(req: express.Request, res: express.Response)  {
 }
 
 export async function apnsReceivedNotification(req: express.Request, res: express.Response) {
-    const body = req.body
+    const body: { [p: string]: string } = req.body
     if (!body?.clientId || !body?.lastSecret) {
         console.log(`Missing key in received notification post: ${JSON.stringify(body)}`)
         res.status(400).send({ status: 'error', reason: 'Invalid post data' });
         return
     }
-    const clientKey = `cli:${body.clientId}`
-    const secretHex = Buffer.from(body.lastSecret, 'base64').toString('hex')
+    const { clientId, lastSecret } = body
+    const clientKey = `cli:${clientId}`
+    const secretHex = Buffer.from(lastSecret, 'base64').toString('hex')
     // see refreshSecret for details of this logic
-    const received: ClientData = { id: body.clientId, secretDate: Date.now(), lastSecret: secretHex }
+    const received: ClientData = { id: clientId, secretDate: Date.now(), lastSecret: secretHex }
     await setClientData(clientKey, received)
     console.log(`Received confirmation of received notification from client ${clientKey}`)
     res.status(204).send()
 }
 
 export async function pubSubTokenRequest(req: express.Request, res: express.Response) {
-    const body = req.body
+    const body: { [p: string]: string } = req.body
     if (!body?.clientId || !body?.activity || !body?.publisherId) {
         console.log(`Missing key in pub-sub token request body: ${JSON.stringify(body)}`)
         res.status(400).send({ status: 'error', reason: 'Invalid post data' });
         return
     }
-    const clientKey = `cli:${body.clientId}`
+    const { clientId, activity, publisherId } = body
+    const clientKey = `cli:${clientId}`
     console.log(`Token request received from client ${clientKey}`)
-    if (body?.userName) {
-        const update: ClientData = { id: body.clientId, userName: body?.userName }
-        await setClientData(clientKey, update)
-    }
     const auth = req.header('Authorization')
     if (!auth || !auth.toLowerCase().startsWith('bearer ')) {
         console.log(`Missing or invalid authorization header: ${auth}`)
@@ -83,27 +87,37 @@ export async function pubSubTokenRequest(req: express.Request, res: express.Resp
         res.status(403).send({ status: 'error', reason: 'Invalid authorization' })
         return
     }
-    if (body.activity !== "publish" && body.activity !== "subscribe") {
+    if (activity !== 'publish' && activity !== 'subscribe') {
         console.log(`Publishing and subscribing are the only allowed activities: ${JSON.stringify(body)}`)
         res.status(400).send({ status: 'error', reason: 'Invalid activity' });
         return
     }
-    if (body.clientId === body.publisherId && body?.activity === "subscribe") {
+    if (clientId === publisherId && activity === 'subscribe') {
         console.log(`Self-publishing is not allowed: ${JSON.stringify(body)}`)
         res.status(400).send({ status: 'error', reason: 'Self-publishing is not allowed' });
         return
     }
-    if (body.clientId !== body.publisherId && body?.activity !== "subscribe") {
+    if (clientId !== publisherId && activity !== 'subscribe') {
         console.log(`Publishing as someone else is not allowed: ${JSON.stringify(body)}`)
         res.status(400).send({ status: 'error', reason: 'Impersonation is not allowed' });
         return
     }
-    if (body.activity == "publish") {
-        const tokenRequest = await createAblyPublishTokenRequest(body.clientId)
+    const existing = await getClientData(clientKey)
+    if (body?.userName && body.userName !== existing?.userName) {
+        if (activity === 'publish') {
+            console.log(`Updating whisperer name from request`)
+        } else {
+            console.log(`Updating listener name from request`)
+        }
+        const update: ClientData = { id: clientId, userName: body?.userName }
+        await setClientData(clientKey, update)
+    }
+    if (activity === 'publish') {
+        const tokenRequest = await createAblyPublishTokenRequest(clientId)
         console.log(`Issued publish token request to client ${clientKey}`)
         res.status(200).send({ status: 'success', tokenRequest: JSON.stringify(tokenRequest)})
     } else {
-        const tokenRequest = await createAblySubscribeTokenRequest(body.clientId, body.publisherId)
+        const tokenRequest = await createAblySubscribeTokenRequest(clientId, publisherId)
         console.log(`Issued subscribe token request to client ${clientKey}`)
         res.status(200).send({ status: 'success', tokenRequest: JSON.stringify(tokenRequest)})
     }
@@ -116,12 +130,24 @@ export async function subscribeToPublisher(req: express.Request, res: express.Re
         res.status(303).send()
         return
     }
+    let clientKey = `cli:${publisherId}`
+    let existing = await getClientData(clientKey)
+    if (!existing) {
+        res.setHeader('Location', '/subscribe404.html')
+        res.status(303).send()
+        return
+    }
+    const publisherName = existing?.userName || 'Anonymous'
     let clientId = req?.session?.clientId
     if (!clientId) {
         clientId = randomUUID()
     }
-    req.session = { publisherId, clientId }
-    const location = `/listen/index.html?clientId=${clientId}&publisherId=${publisherId}`
+    clientKey = `cli:${clientId}`
+    existing = await getClientData(clientKey)
+    const clientName = existing && existing?.userName ? existing.userName : 'Anonymous'
+    req.session = { publisherId, publisherName, clientId, clientName }
+    const queryString = new URLSearchParams(req.session).toString()
+    const location =`/listen/index.html?${queryString}`
     res.setHeader('Location', location)
     res.status(303).send()
 }

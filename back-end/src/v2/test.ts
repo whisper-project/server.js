7ee0a6fd4c6b809cf6b0692a88385f6557371be6
Daniel Brotsky <dan@clickonetwo.io>
2024-01-14 08:30:44 -0800

    Fix tests for app token requests

--- back-end/src/v2/test.ts
+++ back-end/src/v2/test.ts
@@ -3,19 +3,42 @@
 // See the LICENSE file for details.
 
 import {createAblyPublishTokenRequest, createAblySubscribeTokenRequest,} from "./auth.js";
-import {getClientData} from '../client.js'
-import {createTestClient} from '../test.js'
+import {randomUUID} from 'crypto'
+import assert from 'assert'
 
 
 async function testAbly() {
-    const clientKey1 = await createTestClient()
-    const clientData1 = await getClientData(clientKey1)
-    const tokenRequest1 = await createAblyPublishTokenRequest(clientData1!.id)
-    console.log(`Created publish token request: ${JSON.stringify(tokenRequest1)}`)
-    const clientKey2 = await createTestClient()
-    const clientData2 = await getClientData(clientKey2)
-    const tokenRequest2 = await createAblySubscribeTokenRequest(clientData2!.id, clientData1!.id)
-    console.log(`Created subscribe token request: ${JSON.stringify(tokenRequest2)}`)
+    const clientId = randomUUID()
+    const conversationId = randomUUID()
+    const contentId = randomUUID()
+    const pubRequest = await createAblyPublishTokenRequest(clientId, conversationId, contentId)
+    assert(pubRequest.clientId === clientId, "Publish token's client key is wrong")
+    assert(pubRequest.capability[`${conversationId}:control`].contains('publish'),
+        "Publish token doesn't authorize control channel publish")
+    assert(pubRequest.capability[`${conversationId}:control`].contains('subscribe'),
+        "Publish token doesn't authorize control channel subscribe")
+    assert(!pubRequest.capability[`${conversationId}:control`].contains('presence'),
+        "Publish token authorizes control channel presence")
+    assert(pubRequest.capability[`${conversationId}:${contentId}`].contains('publish'),
+        "Publish token doesn't authorize content channel publish")
+    assert(!pubRequest.capability[`${conversationId}:${contentId}`].contains('subscribe'),
+        "Publish token authorizes control content subscribe")
+    assert(!pubRequest.capability[`${conversationId}:${contentId}`].contains('presence'),
+        "Publish token authorizes control channel presence")
+    const subRequest = await createAblySubscribeTokenRequest(clientId, conversationId)
+    assert(subRequest.clientId === clientId, "Subscribe token's client key is wrong")
+    assert(subRequest.capability[`${conversationId}:control`].contains('publish'),
+        "Subscribe token doesn't authorize control channel publish")
+    assert(subRequest.capability[`${conversationId}:control`].contains('subscribe'),
+        "Subscribe token doesn't authorize control channel subscribe")
+    assert(!subRequest.capability[`${conversationId}:control`].contains('presence'),
+        "Subscribe token authorizes control channel presence")
+    assert(!subRequest.capability[`${conversationId}:*`].contains('publish'),
+        "Subscribe token authorizes content channel publish")
+    assert(subRequest.capability[`${conversationId}:*`].contains('subscribe'),
+        "Subscribe token doesn't authorize control content subscribe")
+    assert(!subRequest.capability[`${conversationId}:*`].contains('presence'),
+        "Subscribe token authorizes control channel presence")
 }
 
 export async function testAll(...tests: string[]) {
